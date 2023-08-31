91d8a0c46d22392faa3e61ed290a049317e6426c
Jlouisa <jonathan.louisa@gmail.com>
2023-08-31 10:03:46 +0200

    Build a Node class factory

--- javascript.js
+++ javascript.js
@@ -1,1 +1,21 @@
-
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.leftNode = null;
+    this.rightNode = null;
+  }
+}
+//?+++++++++++++++++++++++++++++
+const first = new Node(1);
+const second = new Node(2);
+const third = new Node(3);
+const fourth = new Node(4);
+const fifth = new Node(5);
+second.leftNode = first;
+second.rightNode = third;
+console.log(first);
+console.log(second);
+console.log(third);
+console.log(fourth);
+console.log(fifth);
+//?+++++++++++++++++++++++++++++
