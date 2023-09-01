e951bc9abd894e1c0ffc384ff3812a1e2472f4ad
Jlouisa <jonathan.louisa@gmail.com>
2023-09-01 10:59:14 +0200

    Add delete function for no-childs and one child

--- javascript.js
+++ javascript.js
@@ -76,7 +76,10 @@ console.log(binaryTree);
 console.log(binaryTree.root);
 console.log("Insert: ");
 console.log(binaryTree.insert(90));
-console.log(binaryTree.delete());
+console.log(binaryTree.insert(6));
+console.log(binaryTree.insert(10));
+// console.log(binaryTree.delete());
+// console.log(binaryTree.delete(67));
 
 //! Visualize the tree in the console
 const prettyPrint = (node, prefix = "", isLeft = true) => {
@@ -96,16 +99,24 @@ prettyPrint(binaryTree.root);
 function deleteSpot(data, _node, next) {
   if (next === null) {
     return console.log("not found");
-  } else if (data === next.data && next.rightNode === null && next.leftNode === null) {
-    console.log("Here on true");
+  } //* Case 1: Delete leaf Nodes
+  else if (data === next.data && next.rightNode === null && next.leftNode === null) {
     _node.rightNode = null;
     _node.leftNode = null;
     return;
-  } else if (data === next.data && next.data > _node.data && next.rightNode !== null) {
-    deleteInnerRight(_node, next);
-  } else if (data === next.data && next.data < _node.data && next.leftNode !== null) {
-    deleteInnerLeft(_node, next);
-  } else {
+  }
+  //* Case 2: Delete Node with one child
+  else if (data === next.data && next.rightNode !== null && next.leftNode === null) {
+    deleteOneChild(_node, next, next.rightNode);
+  } else if (data === next.data && next.leftNode !== null && next.rightNode === null) {
+    deleteOneChild(_node, next, next.leftNode);
+  }
+  //* Case 3: Delete Node with two child
+  else if (data === next.data && next.leftNode !== null && next.rightNode !== null) {
+    deleteTwoChild(_node, next, next.leftNode, next.rightNode);
+  }
+  //* Recurse for to find data
+  else {
     if (data > next.data) {
       _node = next;
       deleteSpot(data, _node, next.rightNode);
@@ -115,29 +126,29 @@ function deleteSpot(data, _node, next) {
     }
   }
 }
-
-function deleteInnerRight(_node, next) {
-  _node.rightNode = next.leftNode;
-  if (next.rightNode !== null) {
-    let rightSpot = next.leftNode.rightNode;
-    while (rightSpot.rightNode !== null) {
-      rightSpot = rightSpot.rightNode;
-    }
-    console.log(next.rightNode);
-    console.log(rightSpot);
-    console.log(rightSpot.rightNode);
-    rightSpot.rightNode = next.rightNode;
+// if (data === next.data && next.data > _node.data && next.rightNode !== null)
+function deleteOneChild(_node, next, child) {
+  if (next.data > _node.data) {
+    _node.rightNode = child;
+    next.leftNode = null;
+    next.right = null;
+    return;
+  } else {
+    _node.leftNode = child;
+    next.leftNode = null;
+    next.right = null;
+    return;
   }
 }
 
-function deleteInnerLeft(_node, next) {
-  console.log(`Head Node: ${_node.data}`);
-  _node.leftNode = next.leftNode;
-  if (next.rightNode !== null) {
-    let rightSpot = next.leftNode.rightNode;
-    while (rightSpot.rightNode !== null) {
-      rightSpot = rightSpot.rightNode;
-    }
-    rightSpot.rightNode = next.rightNode;
-  }
-}
+// if (data === next.data && next.leftNode !== null && next.rightNode !== null)
+function deleteTwoChild(_node, _next, _childLeft, _childRight) {}
+
+//* Case 1: Delete leaf Nodes
+// No problem deleting node. Parent will reference to null
+
+//* Case 2: Delete Node with one child
+// Parent node needs to point to grandchild node
+
+//* Case 3: Delete Node with two child
+// Parent node needs to point to the most left grandchild node
